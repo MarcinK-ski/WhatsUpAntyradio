2ab7e002e383fba38e2400ae7a0ca68c560ce668
Marcin <kalik95@gmail.com>
2017-11-05 20:24:40 +0100

    Update TODO

--- WhatsUpOnAntyradioWPF/WhatsUpOnAntyradioWPF/MainWindow.xaml.cs
+++ WhatsUpOnAntyradioWPF/WhatsUpOnAntyradioWPF/MainWindow.xaml.cs
@@ -2,21 +2,13 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net;
 using System.Data.SQLite;
-using System.Threading;
+
 
 namespace WhatsUpOnAntyradioWPF
 {
